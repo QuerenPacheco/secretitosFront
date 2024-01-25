7c114b74a6be8983e11b0d85272f30b85b343f24
Queren <querenpachecocastro@gmail.com>
2024-01-25 19:24:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Secretos</h1>
+  <LoginForm/>
+</template>
+
+<script>
+import LoginForm from "./components/LoginForm"
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm,
+  }
+}
+</script>
+
+<style>
+</style>
